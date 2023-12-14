cc800267ff9e992996b754c308e6bed7557d86b2
sawallaz <153788222+sawallaz@users.noreply.github.com>
2023-12-14 21:18:48 +0700

    Add files via upload

---
+++ 231031020-sawal-latihan1.py
@@ -0,0 +1,7 @@
+nilai = int(input("Masukkan nilai (0-100): "))
+batas_lulus = 65
+
+if nilai > batas_lulus:
+    print('Selamat, kamu lulus karena pintar dan rajin!')
+else:
+   print('maaf kamu pemalas jadi tidak lulus')
