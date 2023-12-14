8680ad1b8d0b8be1c5147822759a9f64a5dc7894
sawallaz <153788222+sawallaz@users.noreply.github.com>
2023-12-14 21:11:20 +0700

    Delete 231031020-sawal-latihan1.py

--- 231031020-sawal-latihan1.py
+++
@@ -1,7 +0,0 @@
-nilai = int(input("Masukkan nilai (0-100): "))
-batas_lulus = 65
-
-if nilai > batas_lulus:
-    print('Selamat, kamu lulus karena pintar dan rajin!')
-else:
-   print('maaf kamu pemalas jadi tidak lulus')
