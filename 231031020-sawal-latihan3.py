5d5e7205593640444204a36077bd729268d0c075
sawallaz <153788222+sawallaz@users.noreply.github.com>
2023-12-14 10:12:11 +0700

    Delete 231031020-sawal-latihan3.py

--- 231031020-sawal-latihan3.py
+++
@@ -1,9 +0,0 @@
-nama = input("Nama karyawan: ")
-pendapatan = int(input("Pendapatan: "))
-print("Nama karyawan:", nama)
-print("Pendapatan:", pendapatan)
-
-if pendapatan > 1000:
-    print("Status: Berhak")
-else:
-    print("Status: Tidak Berhak")
